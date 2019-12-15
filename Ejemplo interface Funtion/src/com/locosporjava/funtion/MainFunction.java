294049f1e624a940889c57935264d5e0d7c6fb77
salvador <sgzurita@gmail.com>
2019-12-15 19:43:00 +0100

    Commit inicial ejemplo Optional

--- Ejemplo interface Funtion/src/com/locosporjava/funtion/MainFunction.java
+++ Ejemplo interface Funtion/src/com/locosporjava/funtion/MainFunction.java
@@ -3,12 +3,30 @@ package com.locosporjava.funtion;
 import java.util.function.Function;
 
 public class MainFunction {
+	
 	public static void main(String[] args) {
 		int numArticulos=10;
-		Articulo pan=new Articulo("pan",0.60F); 
+		PuntoVenta puntoVenta=new PuntoVenta("OkPan","El Cairo");
+		Articulo pan=new Articulo("pan",0.60F,puntoVenta); 
 	    Function<Articulo, Float> precio=articulo->articulo.getPrecio();
 		precio=precio.andThen(it->it+numArticulos);
 		System.out.println("el precio final es"+precio.apply(pan));
+		
+		//En el caso que quisieramos el nombre del articulo en
+		Function<Articulo,PuntoVenta> x=art-> art.getPuntoVenta();
+		Function<PuntoVenta,String>pv=art->art.getCiudad();
+		pv.compose((x));
+		System.out.println(pv.compose(x).apply(pan));
+		
+		java.util.function.Consumer<Articulo> consumer=art->{
+			art.setNombre("ok pan "+art.getNombre());
+			art.setPrecio(art.getPrecio()*1.10F);
+		};
+		
+		consumer.accept(pan);
+		System.out.println(pan.getNombre()+" "+pan.getPrecio()
+		);
+		
 
 	}
 }
